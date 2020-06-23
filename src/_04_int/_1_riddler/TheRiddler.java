473e497ae35cf43ec026057d4ac2969d491f7ea3
root <root@d89bf8a65a1e>
2020-06-23 23:57:15 +0000

    unbirthday, messages, riddle

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,7 +2,7 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
@@ -11,9 +11,12 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+	String riddle=JOptionPane.showInputDialog("What walks on 4 legs in the morning, two legs during the day, and three legs at night?");	
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if() {
+		JOptionPane.showMessageDialog(null, "Correct!!");
+			
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
